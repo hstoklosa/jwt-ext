d85fba7b9075672d9eb53fd71a9e43f8499963a2
hstoklosa <hubert.stoklosa23@gmail.com>
2025-01-30 03:56:14 +0000

    create base setup for maven project

---
+++ src/test/java/com/jwtext/AppTest.java
@@ -0,0 +1,20 @@
+package com.jwtext;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
